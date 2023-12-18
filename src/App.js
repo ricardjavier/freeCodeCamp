9ed9079ba10e4d455fc0af65815ac00a474665af
ricardjavier <rmantilla00@gmail.com>
2023-12-18 17:36:22 -0300

    Se Crearon los objetos para luego exportarlos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import TestimonioCompente from './Componentes/Testimonio';
-
-
-
+import { testimonioShawn, testimonioSarah, testimonioEmma } from './Componentes/Testimonio';
 
 function App() {
   return (
@@ -10,35 +8,13 @@ function App() {
       <div className='contenedor-principal'>
         <h1>Here is what our alumni say about freeCodeCamp:</h1>
         
-        <TestimonioCompente
-          nombre='Shawn Wang'
-          pais='Singapur'
-          imagen='shawn'
-          cargo='Ingeniero de Software'
-          empresa='Amazon'
-          Testimonio="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer.freeCodeCamp changed my life"
-          negrillas='freeCodeCamp changed my life'/>
-
-        <TestimonioCompente
-          nombre='Sarah Chima'
-          pais='Nigeria'
-          imagen='sarah'
-          cargo='Ingeniera de Software'
-          empresa='ChatDesk'
-          Testimonio="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company." 
-          negrillas='freeCodeCamp was the gateway to my career'/>
-
-        <TestimonioCompente
-          nombre='Emma Bostian'
-          pais='Suecia'
-          imagen='emma'
-          cargo='Ingeniera de Software'
-          empresa='Spotify'
-          Testimonio="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
-          negrillas='freeCodeCamp gave me the skills' />
+        <TestimonioCompente {...testimonioShawn} />
+        <TestimonioCompente {...testimonioSarah} />
+        <TestimonioCompente {...testimonioEmma} />
       </div>
     </div>
   );
 }
 
 export default App;
+
--- src/Componentes/Testimonio.jsx
+++ src/Componentes/Testimonio.jsx
@@ -50,3 +50,32 @@ function TestimonioComponente(props) {
 
 export default TestimonioComponente;
 
+export const testimonioShawn = {
+  nombre: 'Shawn Wang',
+  pais: 'Singapur',
+  imagen: 'shawn',
+  cargo: 'Ingeniero de Software',
+  empresa: 'Amazon',
+  Testimonio: "It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer.freeCodeCamp changed my life",
+  negrillas: 'freeCodeCamp changed my life'
+};
+
+export const testimonioSarah = {
+  nombre: 'Sarah Chima',
+  pais: 'Nigeria',
+  imagen: 'sarah',
+  cargo: 'Ingeniera de Software',
+  empresa: 'ChatDesk',
+  Testimonio: "freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company.",
+  negrillas: 'freeCodeCamp was the gateway to my career'
+};
+
+export const testimonioEmma = {
+  nombre: 'Emma Bostian',
+  pais: 'Suecia',
+  imagen: 'emma',
+  cargo: 'Ingeniera de Software',
+  empresa: 'Spotify',
+  Testimonio: "I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify.",
+  negrillas: 'freeCodeCamp gave me the skills'}
+  
